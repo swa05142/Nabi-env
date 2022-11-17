e4e2b25775d461eb37e0842f5bc504512500c3dd
swa05142 <swa05142@naver.com>
2022-11-17 20:36:03 +0900

    first Commit

--- src/main.js
+++ src/main.js
@@ -1,14 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router/index.js";
-import DateRangePicker from "vue-mj-daterangepicker";
-import "vue-mj-daterangepicker/dist/vue-mj-daterangepicker.css";
 import vuetify from "./plugins/vuetify";
 import { store } from "./store/store.js";
 import VueMoment from "vue-moment";
 
 Vue.config.productionTip = false;
-Vue.use(DateRangePicker);
 Vue.use(VueMoment);
 
 new Vue({
